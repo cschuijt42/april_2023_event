850ecd428957d01ab964d5a0ba17e112d7926655
Casper Schuijt <casperschuijt@gmail.com>
2023-04-02 23:37:11 +0200

    attachment for email

--- ex12/email.rb
+++ ex12/email.rb
@@ -2,6 +2,13 @@ require 'date'
 require 'net/smtp'
 require 'io/console'
 
+# temporary app password: esvmcewuihiiknmb
+
+filename = "norminet.jpg"
+file_content = File.read(filename)
+encoded_content = [file_content].pack("m")
+marker = "UNIQUEMARKER"
+
 print "Your name: "
 origin = gets(chomp: true)
 
@@ -23,10 +30,24 @@ Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', user: email, secret: passwor
 From: #{origin} <#{email}>
 To: #{recipient} <#{recipient_email}>
 Subject: Message from script
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary = #{marker}
 Date: #{Time.now.strftime("%a, %d %b %Y %H:%M:%S %z")}
+--#{marker}
+Content-Type: text/html
+Content-Transfer-Encoding:8bit
 
 This message was sent from cschuijt's script:
+
 #{gets(chomp: true)}
+
+--#{marker}
+Content-Type: multipart/mixed; name = "#{filename}"
+Content-Transfer-Encoding:base64
+Content-Disposition: attachment; filename = "#{filename}"
+
+#{encoded_content}
+--#{marker}--
 END_OF_MESSAGE
 
   smtp.send_message content, email, recipient_email
